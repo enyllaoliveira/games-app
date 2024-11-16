e5deeeb7b31d55bf719c85dfc1e7b972c458a5ef
enyllaoliveira <enyllaoliveira@gmail.com>
2024-11-16 17:14:05 -0300

    header component

--- src/app/components/NavBar/index.tsx
+++ src/app/components/NavBar/index.tsx
@@ -3,35 +3,59 @@ import { HomeIcon } from "../Icons/icons/HomeIcon";
 import { GameIcon } from "../Icons/icons/GameIcon";
 import { MedalIcon } from "../Icons/icons/MetalIcon";
 import { LocationIcon } from "../Icons/icons/LocationIcon";
+import { ReactNode } from "react";
 
+interface NavBarListProps {
+  children: ReactNode;
+  className?: string;
+}
+
+const NavBarList = ({ children, className }: NavBarListProps) => {
+  return (
+    <ul
+      className={`my-4 items-start border-t border-indigo-300/40 hover:border-indigo-300/90 flex flex-col gap-2 ${className}`}
+    >
+      {children}
+    </ul>
+  );
+};
+
+const NavBarListItem = ({ children }: { children: ReactNode }) => {
+  return (
+    <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer flex gap-2 items-center">
+      {" "}
+      {children}
+    </li>
+  );
+};
 export default function Navbar() {
   return (
     <nav className="flex flex-col bg-slate-900 border-r border-indigo-300/40 hover:border-indigo-300/90 text-white w-72 p-2 h-screen">
-      <ul className="flex-grow my-4">
-        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer flex gap-2 items-center">
+      <NavBarList className="flex-grow">
+        <NavBarListItem>
           {" "}
           <HomeIcon className="size-5" />
           Home
-        </li>
-        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer flex gap-2 items-center">
+        </NavBarListItem>
+        <NavBarListItem>
           {" "}
           <GameIcon className="size-5" /> Games
-        </li>
-        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer flex gap-2 items-center">
+        </NavBarListItem>
+        <NavBarListItem>
           {" "}
           <MedalIcon className="size-5" /> Top 10
-        </li>
-        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer flex gap-2 items-center">
+        </NavBarListItem>
+        <NavBarListItem>
           {" "}
           <LocationIcon className="size-5" /> Walkthroughs
-        </li>
-      </ul>
-      <ul className="my-4 border-t border-indigo-300/40 hover:border-indigo-300/90 flex gap-2 items-center">
-        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer flex gap-2 items-center">
+        </NavBarListItem>
+      </NavBarList>
+      <NavBarList>
+        <NavBarListItem>
           <UserIcon className="size-5" />
           Home
-        </li>
-      </ul>
+        </NavBarListItem>
+      </NavBarList>
     </nav>
   );
 }
