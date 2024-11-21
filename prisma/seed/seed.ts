d964d3a4ca10a6dfeb9db3a2a33790783dd8e152
enyllaoliveira <enyllaoliveira@gmail.com>
2024-11-21 06:14:54 -0300

    import article datafrom json file

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,8 +1,37 @@
 import { PrismaClient } from "@prisma/client";
+import articles from "../../src/data/articles.json";
+import { slugify } from "../../src/helpers/slugify";
 
 const prisma = new PrismaClient();
+
+const IsDev = process.env.NODE_ENV === "development";
 async function main() {
   console.log("DB seed");
+  await sendArticle();
+}
+
+async function sendArticle() {
+  if (!IsDev) {
+    throw new Error("Node env is not a development environment");
+  }
+  await prisma.article.deleteMany();
+  await prisma.$executeRawUnsafe(
+    "DELETE FROM SQLITE_SEQUENCE WHERE name=$1",
+    "Article"
+  );
+  for (const article of articles) {
+    const record = await prisma.article.create({
+      data: {
+        title: article.title,
+        slug: slugify(article.title),
+        excerpt: article.excerpt,
+        content: article.content,
+        image: article.image,
+        publishedAt: new Date(article.publish_date),
+      },
+    });
+    console.log("*** created article", record.id, record.title);
+  }
 }
 
 main()
---
+++ src/helpers/slugify.ts
@@ -0,0 +1,8 @@
+import { convert } from "url-slug";
+export const slugify = (value: string) => {
+  return convert(value, {
+    dictionary: {
+      "'": "",
+    },
+  });
+};
